e66637c87fe9582a801148b5627fe7e4244d3f00
lixiaohei <lixiaohei@163.com>
2018-07-26 21:46:28 +0800

    完成首页数据的渲染,以及懒加载

--- src/main.js
+++ src/main.js
@@ -5,12 +5,30 @@ import App from './App.vue'
 import VueRouter from 'vue-router'
 // 引入index组件
 import index from './components/index.vue'
+// 导入ui框架
+import ElementUI from "element-ui";
+// 导入css
+import "element-ui/lib/theme-chalk/index.css";
+// 导入懒加载插件
+import VueLazyload from "vue-lazyload";
+
+
+
 // 使用路由中间件
 Vue.use(VueRouter);
+// 使用ui中间件
+Vue.use(ElementUI);
+// 使用懒加载路由中间件
+Vue.use(VueLazyload,{
+  loading:require('./img/02.gif')
+});
+
+
 
 // 2. 定义路由
 const routes = [
-  { path: '/', component: index }
+  { path: '/', component: index },
+  { path: '/index', component: index }
 ]
 
 // 3. 创建 router 实例，然后传 `routes` 配置
@@ -23,7 +41,7 @@ import './assets/statics/site/css/style.css';
 
 Vue.config.productionTip = false;
 
-const app = new Vue({
+new Vue({
   el: '#app',
   router,
   render: h => h(App)
